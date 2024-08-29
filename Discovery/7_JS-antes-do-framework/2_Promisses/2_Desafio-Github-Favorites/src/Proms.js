139c50f8357ab235b0a8da06d13fb9d98ad6bfd5
Vitor <vitor.malmsteen@gmail.com>
2024-08-29 02:41:05 -0300

    doing

--- Discovery/7_JS-antes-do-framework/2_Promisses/2_Desafio-Github-Favorites/src/Proms.js
+++ Discovery/7_JS-antes-do-framework/2_Promisses/2_Desafio-Github-Favorites/src/Proms.js
@@ -9,32 +9,52 @@ export class LowerClass extends higherClass {
         super()
         this.inputValue()
         this.clearRows()
-        this.parseElement()
     }
-    
+
 
     inputValue() {
         const buttonTag = this.fullBody.querySelector('header button')
-        buttonTag.addEventListener('click', (event) => {
+        buttonTag.addEventListener('click', () => {
             const inputTag = this.fullBody.querySelector('header input')
-            console.log(inputTag.value)
             this.getUserData(inputTag.value)
         })
     }
 
+
     getUserData(username) {
         const url = `https://api.github.com/users/${username}`
-        const res = fetch(url)
-                        .then(req => req.json())
-                        .then(jsonObj => {
-                            const { name, public_repos, followers, login } = jsonObj
-                            console.log(name, public_repos, followers, login)
-                            return { name, public_repos, followers, login }
-                        })
-                        return res
+        const resPromise = fetch(url)
+                            .then(req => req.json())
+                            .then(jsonObj => {
+                                const { name, public_repos, followers, login } = jsonObj
+                                console.log(name, public_repos, followers, login)
+                                this.parseElement(name, public_repos, followers, login)
+                            })
+        return resPromise
     }
 
-    
+
+    deleteEntryFeature(entry) {
+        const removeBtn = entry.querySelector('.fourthDim button')
+
+        removeBtn.addEventListener('click', () => {
+            const removingUser = entry.querySelector('.NameAndAdr a').textContent
+
+            this.clearRows()
+
+            const bkDatas = JSON.parse(localStorage.getItem('usersLocalBK'))
+            bkDatas.forEach(bkData => {
+                if (bkData.login !== removingUser) {
+                    this.parseElement(
+                        bkData.name, 
+                        bkData.repos, 
+                        bkData.followers, 
+                        bkData.login.replace('/','')
+                    )
+                }
+            })
+        })
+    }
 
     clearRows() {
         const rows = this.fullBody.querySelectorAll('tbody tr')
@@ -44,29 +64,61 @@ export class LowerClass extends higherClass {
     }
 
 
-    parseElement() {
+    parseElement(ghname, ghrepos, ghfollowers, ghlogin) {
         const tableBody = this.fullBody.querySelector('tbody')
-        const newRow = document.createElement('tr')     // tableBody.createElement('tr')
+        const newRow = document.createElement('tr')
         
         const element = `
         <td class="firstDim">
                         <div class="conteiner">
-                            <img src="https://github.com/vihmalmsteen.png">
+                            <img src="https://github.com/${ghlogin}.png">
                             <div class="NameAndAdr">
-                                <span>Vih Malmsteen</span>
-                                <a href="https://github.com/vihmalmsteen" target="_blank">/vihmalmsteen</a>
+                                <span>${ghname}</span>
+                                <a href="https://github.com/${ghlogin}" target="_blank">/${ghlogin}</a>
                             </div>
                         </div>
                     </td>
-                    <td class="secondDim">123</td>
-                    <td class="thirdDim">1234</td>
+                    <td class="secondDim">${ghrepos}</td>
+                    <td class="thirdDim">${ghfollowers}</td>
                     <td class="fourthDim">
                         <button>Remover</button>
                     </td>`
-                    newRow.innerHTML = element
-                    console.log(newRow)
-                    tableBody.append(newRow)
+
+        newRow.innerHTML = element
+        console.log(newRow)
+        this.deleteEntryFeature(newRow)
+        tableBody.prepend(newRow)
+        this.setLocals()
     }
+    
+    
+    setLocals() {
+        const rowsEntries = this.fullBody.querySelectorAll('tbody tr')
+        
+        let list = []
+
+        rowsEntries.forEach(rowEntry => {
+
+            const entryUsername = rowEntry.querySelector('.NameAndAdr span').textContent
+            const entryLogin = rowEntry.querySelector('.NameAndAdr a').textContent
+            const entryrepos = rowEntry.querySelector('.secondDim').textContent
+            const entryFollows = rowEntry.querySelector('.thirdDim').textContent
+
+            const entryObj = {
+                name:entryUsername,
+                login:entryLogin,
+                repos:entryrepos,
+                followers:entryFollows
+            }
+
+            list.push(entryObj)
+        });
+
+        localStorage.setItem('usersLocalBK', JSON.stringify(list))
+    }
+
+
+
 
 
     // getUserData() {
